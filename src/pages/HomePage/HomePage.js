be00cdaf04d23054f0975279394e30ba0a16c234
Felix Chen <fc37@njit.edu>
2024-04-13 19:05:18 -0400

    Homepage progress #1

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import './HomePage.css';
 
 function HomePage() {
+  const message = "Welcome to StudyPilot";
+
   return (
     <div className="welcome_page">
       <div className="page-header">
-        <h1 className='welcome-title'>StudyPilot</h1>
+        <h1 className='welcome-title' id="header">{message}</h1>
       </div>
     </div>
   );
