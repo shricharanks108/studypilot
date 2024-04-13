308fdbad1ea9a753f6760a6fbc8c9859bd386fe9
Shricharan K.S. <shricharanks108@gmail.com>
2024-04-12 23:25:00 -0400

    Create HomePage.js

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="welcome_page">
+      <div className="page-header">
+        <h1 className='welcome-title'>StudyPilot</h1>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
