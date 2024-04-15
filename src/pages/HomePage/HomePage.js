39ed6d8d4451c0e4dddee8505882be0eb0e3a6e9
Felix Chen <fc37@njit.edu>
2024-04-15 16:54:27 -0400

    Homepage improvements

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,19 @@
 import './HomePage.css';
+import logo from './book.svg';
 
 function HomePage() {
   return (
-    <div className="welcome_page">
-      <div className="page-header">
-        <h1 className='welcome-title'>StudyPilot</h1>
-      </div>
+    <div className="welcome_page" style={{ textAlign: 'center' }}>
+      <h1 className='Welcome'>introducing StudyPilot</h1>
+      <img
+        alt=""
+        src={logo}
+        width="700"
+        height="500"
+        className="book"
+      />
+      <p className="plans">Flexible plans that fit your needs</p>
+      <p className="why">Why StudyPilot?</p>
     </div>
   );
 }
