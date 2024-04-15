de7cc9c916a43a0fa69c799054f4d4bdd9b359df
Felix Chen <fc37@njit.edu>
2024-04-15 17:34:11 -0400

    Homepage Improvements pt. 2

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import './HomePage.css';
+import './HomePage.css'; // Make sure this path is correct
 import Lottie from 'react-lottie';
-import animationData from './study.json'; // Import your Lottie animation JSON file
+import Button from 'react-bootstrap/Button';
+import animationData from './2people.json';
 
 function HomePage() {
   return (
-    <div className="welcome_page" style={{ textAlign: 'center' }}>
-      <h1 className='Welcome'>introducing StudyPilot</h1>
-      <Lottie
-        options={{
-          loop: true,
-          autoplay: true,
-          animationData: animationData,
-        }}
-        height={600}
-        width={800}
-      />
-      <p className="plans">Flexible plans that fit your needs</p>
-      <p className="why">Why StudyPilot?</p>
+    <div className="welcome_page" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <div style={{ flex: 1 }}>
+        <Lottie
+          options={{
+            loop: true,
+            autoplay: true,
+            animationData: animationData,
+          }}
+          height={400}
+          width={400}
+        />
+      </div>
+      <div style={{ flex: 1, textAlign: 'left', paddingLeft: '50px' }}>
+        <h1 className='Welcome'>Introducing StudyPilot!</h1>
+        <p className="slogan">Learning takes flight</p>
+        <p className="why">Why StudyPilot?</p>
+        <Button className='learn-more-button' href="/about-us">Learn More</Button>
+        <Button className='learn-more-button' href="/sign-up">Sign Up</Button>
+      </div>
     </div>
   );
 }
