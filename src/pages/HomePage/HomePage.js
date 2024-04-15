eeb0796bc28b0c8e535274df15cab31764362e5b
Felix Chen <fc37@njit.edu>
2024-04-15 17:04:16 -0400

    Added nice little graphic

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,16 +1,20 @@
+import React from 'react';
 import './HomePage.css';
-import logo from './book.svg';
+import Lottie from 'react-lottie';
+import animationData from './study.json'; // Import your Lottie animation JSON file
 
 function HomePage() {
   return (
     <div className="welcome_page" style={{ textAlign: 'center' }}>
       <h1 className='Welcome'>introducing StudyPilot</h1>
-      <img
-        alt=""
-        src={logo}
-        width="700"
-        height="500"
-        className="book"
+      <Lottie
+        options={{
+          loop: true,
+          autoplay: true,
+          animationData: animationData,
+        }}
+        height={600}
+        width={800}
       />
       <p className="plans">Flexible plans that fit your needs</p>
       <p className="why">Why StudyPilot?</p>
