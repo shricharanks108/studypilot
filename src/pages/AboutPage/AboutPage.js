e4f259dc69db5c8ec90d8ce68e39d3a831dcc79e
Felix Chen <fc37@njit.edu>
2024-04-16 21:49:56 -0400

    Updated About Us Page

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,8 +1,21 @@
-// Temporary JavaScript code
+import React from 'react';
+import './AboutPage.css';
+import PeopleIcon from './people.svg';
 
-// Define some variables
-let greeting = "Hello";
-let name = "World";
+function App() {
+  return (
+    <div className="container">
+      <div className="icon-text-container">
+        <img src={PeopleIcon} alt="People" className="icon" />
+        <div>
+          <h1>About Us</h1>
+          <p>We are a group of students in New Jersey Institute of Technology's CS 485 with Dr. Suresh Kumar building out our product, StudyPilot! This was a 
+              semester-long project completed in collaboration with Palak A, Felix C, Vidhi D, Shricharan KS 🚀.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-// Concatenate and log a message
-console.log(greeting + ", " + name + "!");
+export default App;
