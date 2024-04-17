e235c4bbb1b8d7adfddb340e28c6151ed88ada8e
Felix Chen <fc37@njit.edu>
2024-04-16 22:36:45 -0400

    Update AboutPage.js w/ Punctuation

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -10,7 +10,7 @@ function App() {
         <div>
           <h1>About Us</h1>
           <p>We are a group of students in New Jersey Institute of Technology's CS 485 with Dr. Suresh Kumar building out our product, StudyPilot! This was a 
-              semester-long project completed in collaboration with Palak A, Felix C, Vidhi D, Shricharan KS 🚀.
+              semester-long project completed in collaboration with Palak A, Felix C, Vidhi D, and Shricharan KS 🚀.
           </p>
         </div>
       </div>
