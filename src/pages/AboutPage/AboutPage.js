c85a6a0042f3e73a3c296587777f09f51fc34419
Felix Chen <fc37@njit.edu>
2024-04-16 23:02:36 -0400

    Fix for pricing page alignment

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -2,20 +2,22 @@ import React from 'react';
 import './AboutPage.css';
 import PeopleIcon from './people.svg';
 
-function App() {
+function AboutUs() {
   return (
-    <div className="container">
-      <div className="icon-text-container">
-        <img src={PeopleIcon} alt="People" className="icon" />
-        <div>
-          <h1>About Us</h1>
-          <p>We are a group of students in New Jersey Institute of Technology's CS 485 with Dr. Suresh Kumar building out our product, StudyPilot! This was a 
-              semester-long project completed in collaboration with Palak A, Felix C, Vidhi D, and Shricharan KS 🚀.
-          </p>
+    <div className="about-page-container"> {/* Added a unique parent class */}
+      <div className="container">
+        <div className="icon-text-container">
+          <img src={PeopleIcon} alt="People" className="icon" />
+          <div>
+            <h1>About Us</h1>
+            <p>We are a group of students in New Jersey Institute of Technology's CS 485 with Dr. Suresh Kumar building out our product, StudyPilot! This was a 
+                semester-long project completed in collaboration with Palak A, Felix C, Vidhi D, and Shricharan KS 🚀.
+            </p>
+          </div>
         </div>
       </div>
     </div>
   );
 }
 
-export default App;
+export default AboutUs;
