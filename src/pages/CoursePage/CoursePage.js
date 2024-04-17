d1efbfb14077574f8948cbb0adc383de0731da12
palakagr34 <palakagr34@gmail.com>
2024-04-16 21:12:41 -0400

    chapter allignment

--- src/pages/CoursePage/CoursePage.js
+++ src/pages/CoursePage/CoursePage.js
@@ -17,27 +17,41 @@ function CoursePage() {
 
       <div class="chapters">
         <h2>Chapters:</h2>
-        <ul>
-            <li><Link to="/chapterPage"><span style={{ marginLeft: '20px' }}>1.  Tour of Computer Systems</span><span style={{ marginLeft: '50px' }}>by 4/18</span></Link></li>
-            <li><Link to="/chapterPage"><span style={{ marginLeft: '20px' }}>2.  Representing and Manipulating Information</span><span style={{ marginLeft: '50px' }}>by 4/25</span></Link></li>
-            <li><Link to="/chapterPage"><span style={{ marginLeft: '20px' }}>3.  Machine Level Representation</span><span style={{ marginLeft: '50px' }}>by 4/30</span></Link></li>
-            <li><Link to="/chapterPage"><span style={{ marginLeft: '20px' }}>4.  Processor Architecture</span><span style={{ marginLeft: '50px' }}>by 4/30</span></Link></li>
-            <li><Link to="/chapterPage"><span style={{ marginLeft: '20px' }}>5.  Optimizing Program Performance</span><span style={{ marginLeft: '50px' }}>by 4/30</span></Link></li>
-            <li><Link to="/chapterPage"><span style={{ marginLeft: '20px' }}>6.  The Memory Heirarchy</span><span style={{ marginLeft: '50px' }}>by 4/30</span></Link></li>
-            <li><Link to="/chapterPage"><span style={{ marginLeft: '20px' }}>7.  Linking</span><span style={{ marginLeft: '50px' }}>by 4/30</span></Link></li>
-            <li><Link to="/chapterPage"><span style={{ marginLeft: '20px' }}>8.  Exceptional Control Flow</span><span style={{ marginLeft: '50px' }}>by 4/30</span></Link></li>
-            <li><Link to="/chapterPage"><span style={{ marginLeft: '20px' }}>9.  Virtual Memory</span><span style={{ marginLeft: '50px' }}>by 4/30</span></Link></li>
-            <li><Link to="/chapterPage"><span style={{ marginLeft: '20px' }}>10. System Level I/O</span><span style={{ marginLeft: '50px' }}>by 4/30</span></Link></li>
-            <li><Link to="/chapterPage"><span style={{ marginLeft: '20px' }}>11. Network Programming</span><span style={{ marginLeft: '50px' }}>by 4/30</span></Link></li>
-            <li><Link to="/chapterPage"><span style={{ marginLeft: '20px' }}>12. Concurrent Programming</span><span style={{ marginLeft: '50px' }}>by 4/30</span></Link></li>
-        </ul>
-      </div>
+        <div class="chapter-list">
+            <div class="chapter-name"><Link to="/chapterPage">1. Tour of Computer Systems</Link></div>
+            <div class="chapter-date">by 1/18</div>
+            <div class="chapter-name"><Link to="/chapterPage">2. Representing and Manipulating Information</Link></div>
+            <div class="chapter-date">by 1/25</div>
+            <div class="chapter-name"><Link to="/chapterPage">3. Machine Level Representation</Link></div>
+            <div class="chapter-date">by 1/30</div>
+            <div class="chapter-name"><Link to="/chapterPage">4. Processor Architecture</Link></div>
+            <div class="chapter-date">by 2/14</div>
+            <div class="chapter-name"><Link to="/chapterPage">5. Optimizing Program Performance</Link></div>
+            <div class="chapter-date">[not needed]</div>
+            <div class="chapter-name"><Link to="/chapterPage">6. The Memory Heirarchy</Link></div>
+            <div class="chapter-date">by 2/21</div>
+            <div class="chapter-name"><Link to="/chapterPage">7. Linking</Link></div>
+            <div class="chapter-date">[not needed]</div>
+            <div class="chapter-name"><Link to="/chapterPage">8. Exceptional Control Flow</Link></div>
+            <div class="chapter-date">[not needed]</div>
+            <div class="chapter-name"><Link to="/chapterPage">9. Virtual Memory</Link></div>
+            <div class="chapter-date">by 2/30</div>
+            <div class="chapter-name"><Link to="/chapterPage">10. System Level I/O</Link></div>
+            <div class="chapter-date">[not needed]</div>
+            <div class="chapter-name"><Link to="/chapterPage">11. Network Programming</Link></div>
+            <div class="chapter-date">[not needed]</div>
+            <div class="chapter-name"><Link to="/chapterPage">12. Concurrent Programming</Link></div>
+            <div class="chapter-date">by 3/7</div>
+        </div>
+        </div>
+
+
 
       <div class="exams">
         <h2>Exams:</h2>
         <ul>
-          <li><Link to="/examPage"><span style={{ marginLeft: '20px' }}></span>Midterm 1 (Ch 1-3) <span style={{ marginLeft: '50px' }}></span>4/26</Link></li>
-          <li><Link to="/examPage"><span style={{ marginLeft: '20px' }}></span>Midterm 2 (Ch 4-7) <span style={{ marginLeft: '50px' }}></span>5/10</Link></li>
+          <li><Link to="/examPage"><span style={{ marginLeft: '20px' }}></span>Midterm (Ch 1-3) <span style={{ marginLeft: '290px' }}></span>4/26</Link></li>
+          <li><Link to="/examPage"><span style={{ marginLeft: '20px' }}></span>Final (Ch 4,6,9,12) <span style={{ marginLeft: '288px' }}></span>5/10</Link></li>
         </ul>
       </div>
     </div>
